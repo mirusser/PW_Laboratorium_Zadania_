18bfca650079a5e4122f554d5ac3d863328bd675
Mirek <mirusser@gmail.com>
2016-06-17 14:36:06 +0200

    Zad 1.2
    Jaka może być maksymalna liczba utworzonych wątków w systemie?

---
+++ PW_1_2/PW_1_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace PW_1_2
+{
+    class Program
+    {
+        static void Metoda()
+        {
+            Thread.Sleep(2147483647);
+            //watek musi być uśpiony, robić cokolwiek, ponieważ inaczej, 
+            //zaraz po zakończeniu tej metody jest usuwany
+        }
+
+
+        static void Main()
+        {
+            int liczba_wątków = 0;
+
+            Console.WriteLine("Zaczynam tworzenie wątków.");
+            DateTime start = DateTime.Now;
+            while (true)
+            {
+                Thread thread = new Thread(Metoda);
+                try
+                {
+                    thread.Start();
+                    liczba_wątków++;
+                }
+                catch
+                {
+                    DateTime end = DateTime.Now;
+                    Console.WriteLine("Czas potrzebny na utworzenie wszystkich wątków: {0}", (end-start));
+                    Console.WriteLine("liczba_wątków wątków: {0}", liczba_wątków);
+                    break;
+                }
+            }
+            Console.WriteLine("Utworzono maksymalną ilość wątków.");
+            Console.ReadLine();//By zobaczyć wynik działania programu
+        }
+    }
+}
