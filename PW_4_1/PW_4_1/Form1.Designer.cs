b6dd5a493fef045ff0bd31ecbb89f1f31815d879
Mirek <mirusser@gmail.com>
2016-06-18 03:13:49 +0200

    Zad 4.1

---
+++ PW_4_1/PW_4_1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PW_4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxPermutations = new System.Windows.Forms.TextBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(242, 21);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(242, 79);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxPermutations
+            // 
+            this.textBoxPermutations.Location = new System.Drawing.Point(3, 129);
+            this.textBoxPermutations.Name = "textBoxPermutations";
+            this.textBoxPermutations.Size = new System.Drawing.Size(385, 20);
+            this.textBoxPermutations.TabIndex = 4;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(3, 181);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(385, 20);
+            this.textBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 353);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.textBoxPermutations);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxPermutations;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ PW_4_1/PW_4_1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PW_4_1
+{
+    public partial class Form1 : Form
+    {
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        bool DisplayArray(ref int[] array)
+        {
+            int zmienna;
+            textBox.Clear();
+            for (int i = 0; i < array.Length; i++)
+            {
+                zmienna = array[i];
+                textBox.Invoke(new Action(delegate ()
+                {
+                    textBox.AppendText(zmienna + " ");
+                    Thread.Sleep(100);
+                }));
+            }
+            return true;
+        }
+        public int Factorial(int n)
+        {
+            int result = 1;
+            if (n == 0 || n == 1)
+            {
+                return result;
+            }
+            else
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            }
+        }
+
+        public void GeneratePermutation(int n)
+        {
+            int factorialOfN = Factorial(n);
+            textBoxPermutations.Invoke(new Action(delegate ()
+            {
+                textBox.Clear();
+                textBox.AppendText("Liczba wszystkich permutacji:" + factorialOfN);
+            }));
+
+            int[] tabofnumberstopermutate = new int[n]; //this table contains all numbers that will be permutate
+
+            for (int i = 0; i < n; i++)
+            {
+                tabofnumberstopermutate[i] = i+1;
+            }
+            DisplayArray(ref tabofnumberstopermutate);
+            Thread.Sleep(100);
+            PermutationAlgorithm(ref tabofnumberstopermutate, n);
+        }
+
+        public void PermutationAlgorithm(ref int[] tabofnumberstopermutate, int n)
+        {
+            /*
+             *Algorytm ten, zaczerpnięty z wykładu R. Sedgewicka na Uniwersytecie w Princeton 
+            * wymaga 2N! zamian i jest przykładem algorytmu z powrotami.
+            */
+            if (n==1)
+            {
+                DisplayArray(ref tabofnumberstopermutate);
+            }
+            else
+            {
+                for (int i = 0; i < tabofnumberstopermutate.Length; i++)
+                {
+                    if(n>=0)
+                    {
+                        Swap<int>(ref tabofnumberstopermutate[i], ref tabofnumberstopermutate[n - 1]);
+                        DisplayArray(ref tabofnumberstopermutate);
+                        PermutationAlgorithm(ref tabofnumberstopermutate, (n - 1));
+                        Swap<int>(ref tabofnumberstopermutate[i], ref tabofnumberstopermutate[n - 1]);
+                    }
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            GeneratePermutation(Convert.ToInt32(numericUpDown1.Value));
+
+        }
+    }
+}
