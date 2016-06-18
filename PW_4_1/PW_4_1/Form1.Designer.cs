aaa668981bec0568ed217bf72a500a2d9ebd0ebb
Mirek <mirusser@gmail.com>
2016-06-18 16:07:41 +0200

    Zad 4.1

--- PW_4_1/PW_4_1/Form1.Designer.cs
+++ PW_4_1/PW_4_1/Form1.Designer.cs
@@ -32,19 +32,28 @@
             this.buttonStart = new System.Windows.Forms.Button();
             this.textBoxPermutations = new System.Windows.Forms.TextBox();
             this.textBox = new System.Windows.Forms.TextBox();
+            this.labeln = new System.Windows.Forms.Label();
+            this.labelFactorialn = new System.Windows.Forms.Label();
+            this.labelNumberOfSwaps = new System.Windows.Forms.Label();
+            this.textBoxNumberOfSwaps = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelPassedTime = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(242, 21);
+            this.numericUpDown1.Location = new System.Drawing.Point(52, 17);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 2;
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(242, 79);
+            this.buttonStart.Location = new System.Drawing.Point(241, 17);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(75, 23);
             this.buttonStart.TabIndex = 3;
@@ -54,29 +63,123 @@
             // 
             // textBoxPermutations
             // 
-            this.textBoxPermutations.Location = new System.Drawing.Point(3, 129);
+            this.textBoxPermutations.Location = new System.Drawing.Point(52, 47);
             this.textBoxPermutations.Name = "textBoxPermutations";
-            this.textBoxPermutations.Size = new System.Drawing.Size(385, 20);
+            this.textBoxPermutations.Size = new System.Drawing.Size(120, 20);
             this.textBoxPermutations.TabIndex = 4;
             // 
             // textBox
             // 
-            this.textBox.Location = new System.Drawing.Point(3, 181);
+            this.textBox.Location = new System.Drawing.Point(15, 166);
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(385, 20);
+            this.textBox.Size = new System.Drawing.Size(169, 20);
             this.textBox.TabIndex = 5;
+            // 
+            // labeln
+            // 
+            this.labeln.AutoSize = true;
+            this.labeln.Location = new System.Drawing.Point(12, 17);
+            this.labeln.Name = "labeln";
+            this.labeln.Size = new System.Drawing.Size(16, 13);
+            this.labeln.TabIndex = 6;
+            this.labeln.Text = "n:";
+            // 
+            // labelFactorialn
+            // 
+            this.labelFactorialn.AutoSize = true;
+            this.labelFactorialn.Location = new System.Drawing.Point(12, 47);
+            this.labelFactorialn.Name = "labelFactorialn";
+            this.labelFactorialn.Size = new System.Drawing.Size(19, 13);
+            this.labelFactorialn.TabIndex = 7;
+            this.labelFactorialn.Text = "n!:";
+            // 
+            // labelNumberOfSwaps
+            // 
+            this.labelNumberOfSwaps.AutoSize = true;
+            this.labelNumberOfSwaps.Location = new System.Drawing.Point(12, 91);
+            this.labelNumberOfSwaps.Name = "labelNumberOfSwaps";
+            this.labelNumberOfSwaps.Size = new System.Drawing.Size(94, 13);
+            this.labelNumberOfSwaps.TabIndex = 8;
+            this.labelNumberOfSwaps.Text = "Number of Swaps:";
+            // 
+            // textBoxNumberOfSwaps
+            // 
+            this.textBoxNumberOfSwaps.Location = new System.Drawing.Point(15, 118);
+            this.textBoxNumberOfSwaps.Name = "textBoxNumberOfSwaps";
+            this.textBoxNumberOfSwaps.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumberOfSwaps.TabIndex = 9;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 202);
+            this.progressBar1.Maximum = 1000000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(367, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 10;
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(241, 58);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 23);
+            this.buttonPause.TabIndex = 11;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(241, 102);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 12;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Swaps:";
+            // 
+            // labelPassedTime
+            // 
+            this.labelPassedTime.AutoSize = true;
+            this.labelPassedTime.Location = new System.Drawing.Point(19, 240);
+            this.labelPassedTime.Name = "labelPassedTime";
+            this.labelPassedTime.Size = new System.Drawing.Size(48, 13);
+            this.labelPassedTime.TabIndex = 14;
+            this.labelPassedTime.Text = "Passed: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 353);
+            this.ClientSize = new System.Drawing.Size(391, 262);
+            this.Controls.Add(this.labelPassedTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBoxNumberOfSwaps);
+            this.Controls.Add(this.labelNumberOfSwaps);
+            this.Controls.Add(this.labelFactorialn);
+            this.Controls.Add(this.labeln);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.textBoxPermutations);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Permutation";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -88,6 +191,15 @@
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.TextBox textBoxPermutations;
         private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label labeln;
+        private System.Windows.Forms.Label labelFactorialn;
+        private System.Windows.Forms.Label labelNumberOfSwaps;
+        private System.Windows.Forms.TextBox textBoxNumberOfSwaps;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPassedTime;
     }
 }
 
--- PW_4_1/PW_4_1/Form1.cs
+++ PW_4_1/PW_4_1/Form1.cs
@@ -8,33 +8,79 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Diagnostics;
 
 namespace PW_4_1
 {
     public partial class Form1 : Form
     {
-        static void Swap<T>(ref T lhs, ref T rhs)
+        #region Variables
+        Thread PermutationThread;
+        Thread TimeThread;
+        volatile int[] tabofnumberstopermutate;
+        volatile int numberOfSwaps;
+        Stopwatch stoper;
+        #endregion
+
+        #region Time Thread Method
+        void TimeThreadMethod()
+        {
+            stoper.Start();
+            while (true)
+            {
+                MethodInvoker action = delegate { labelPassedTime.Text = "Passed: " + (stoper.ElapsedMilliseconds) + " ms"; };
+                labelPassedTime.Invoke(action);
+                if(progressBar1.Value == numberOfSwaps)
+                {
+                    stoper.Stop();
+                }
+            }
+        }
+        #endregion
+
+        #region Swap methods
+        void QuantityOfSwaps(int number) //number of swaps to be done to fully permutate number
+        {
+            numberOfSwaps = Factorial(number) * 2;
+        }
+        void Swap<T>(ref T lhs, ref T rhs)
         {
             T temp;
             temp = lhs;
             lhs = rhs;
             rhs = temp;
         }
-        bool DisplayArray(ref int[] array)
+        #endregion
+
+        #region Display Swaps and Update Progress Bar
+        void DisplayArrayAndUpdateProgrssBar()
         {
             int zmienna;
-            textBox.Clear();
-            for (int i = 0; i < array.Length; i++)
+
+            textBox.Invoke(new Action(delegate ()
             {
-                zmienna = array[i];
-                textBox.Invoke(new Action(delegate ()
+                textBox.Clear();
+            }));
+
+            if(tabofnumberstopermutate!=null)
+            {
+                for (int i = 0; i < tabofnumberstopermutate.Length; i++)
                 {
-                    textBox.AppendText(zmienna + " ");
-                    Thread.Sleep(100);
-                }));
+                    zmienna = tabofnumberstopermutate[i];
+
+                    MethodInvoker action = delegate { textBox.AppendText(zmienna + " "); };
+                    progressBar1.Invoke(action);
+                }
             }
-            return true;
+            if (progressBar1.Value < numberOfSwaps)
+            {
+                MethodInvoker action1 = delegate { progressBar1.Value++; };
+                progressBar1.Invoke(action1);
+            }
         }
+        #endregion
+
+        #region What is the Factorial?
         public int Factorial(int n)
         {
             int result = 1;
@@ -51,28 +97,31 @@ namespace PW_4_1
                 return result;
             }
         }
+        #endregion
 
-        public void GeneratePermutation(int n)
+        #region Permutation
+        public void GeneratePermutation(object integerN)
         {
+            int n = Convert.ToInt32(integerN); ;
             int factorialOfN = Factorial(n);
             textBoxPermutations.Invoke(new Action(delegate ()
             {
-                textBox.Clear();
-                textBox.AppendText("Liczba wszystkich permutacji:" + factorialOfN);
+                textBoxPermutations.Clear();
+                textBoxPermutations.AppendText(factorialOfN.ToString());
             }));
 
-            int[] tabofnumberstopermutate = new int[n]; //this table contains all numbers that will be permutate
+            tabofnumberstopermutate = new int[n]; //this tab contains all numbers that will be permutate
 
             for (int i = 0; i < n; i++)
             {
                 tabofnumberstopermutate[i] = i+1;
             }
-            DisplayArray(ref tabofnumberstopermutate);
-            Thread.Sleep(100);
-            PermutationAlgorithm(ref tabofnumberstopermutate, n);
+            DisplayArrayAndUpdateProgrssBar();
+            Thread.Sleep(10);
+            PermutationAlgorithm(n);
         }
 
-        public void PermutationAlgorithm(ref int[] tabofnumberstopermutate, int n)
+        public void PermutationAlgorithm(int n)
         {
             /*
              *Algorytm ten, zaczerpnięty z wykładu R. Sedgewicka na Uniwersytecie w Princeton 
@@ -80,32 +129,116 @@ namespace PW_4_1
             */
             if (n==1)
             {
-                DisplayArray(ref tabofnumberstopermutate);
+                DisplayArrayAndUpdateProgrssBar();
             }
             else
             {
                 for (int i = 0; i < tabofnumberstopermutate.Length; i++)
                 {
-                    if(n>=0)
+                    if (n>=0)
                     {
                         Swap<int>(ref tabofnumberstopermutate[i], ref tabofnumberstopermutate[n - 1]);
-                        DisplayArray(ref tabofnumberstopermutate);
-                        PermutationAlgorithm(ref tabofnumberstopermutate, (n - 1));
+
+                        DisplayArrayAndUpdateProgrssBar();
+
+                        PermutationAlgorithm((n - 1));
+
                         Swap<int>(ref tabofnumberstopermutate[i], ref tabofnumberstopermutate[n - 1]);
                     }
+                }
+                DisplayArrayAndUpdateProgrssBar();
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            stoper = new Stopwatch();
+        }
+        #endregion
+
+        #region Buttons
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if(numericUpDown1.Value<=0)
+            {
+                MessageBox.Show("This number is wrong!\nDon't be silly!","Error");
+            }
+            else
+            {
+                //secure if someone click more than one "buttonStart", during permutation
+                if(TimeThread!=null)
+                {
+                    TimeThread.Abort();
+                    TimeThread = null;
+                    stoper.Reset();
+                }
+                if(PermutationThread!=null)
+                {
+                    PermutationThread.Abort();
+                    PermutationThread = null;
+                }
+
+                numberOfSwaps = 0;
+                QuantityOfSwaps((Convert.ToInt32(numericUpDown1.Value)));
+                textBoxNumberOfSwaps.Text = numberOfSwaps.ToString();
+                progressBar1.Maximum = numberOfSwaps;
+                progressBar1.Visible = true;
+                progressBar1.Value = 0;
+
+                TimeThread = new Thread(new ThreadStart(TimeThreadMethod));
+
+                PermutationThread = new Thread(new ParameterizedThreadStart(GeneratePermutation));
+
+                PermutationThread.Priority = ThreadPriority.AboveNormal;
+                PermutationThread.Start((Convert.ToInt32(numericUpDown1.Value)));
+                TimeThread.Start();
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (PermutationThread != null)
+            {
+                if (PermutationThread.ThreadState == System.Threading.ThreadState.Suspended)
+                {
+                    PermutationThread.Resume();
+                }
+                else
+                {
+                    PermutationThread.Suspend();
                 }
             }
         }
 
-        public Form1()
+        private void buttonStop_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            if(PermutationThread!=null && TimeThread!=null)
+            {
+                TimeThread.Abort();
+                PermutationThread.Abort();
+                textBox.Clear();
+                progressBar1.Value = 0;
+                progressBar1.Update();
+                labelPassedTime.Text = "Passed:";
+            }
         }
+        #endregion
 
-        private void buttonStart_Click(object sender, EventArgs e)
+        #region Closing the Program
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            GeneratePermutation(Convert.ToInt32(numericUpDown1.Value));
-
+            if (PermutationThread != null)
+            {
+                PermutationThread.Abort();
+            }
+            if(TimeThread!=null)
+            {
+                TimeThread.Abort();
+            }
         }
+        #endregion
     }
 }
